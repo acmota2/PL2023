abb860cf3f2b227ec13c27baa6e1d11d74132b27
acmota2 <acmota2@gmail.com>
2023-03-09 22:56:08 +0000

    TPC4: ongoing

--- TPC4/csv_utils.py
+++ TPC4/csv_utils.py
@@ -5,13 +5,12 @@ table = {
     'media': lambda l: sum(l) / len(l),
     'sum': lambda l: sum(l),
     'maximum': lambda l: reduce(lambda x, y: x if x > y else y, l),
-    'minimum': lambda l: reduce(lambda x, y: x if x < y else y, l)
+    'minimum': lambda l: reduce(lambda x, y: x if x < y else y, l),
+    '': None
 }
 
-def range_maker(line: str, range) -> list[str]:
-    ...
-
-def make_match(header: str):
+def make_match(header: str) -> str:
+    regex = r''
     for field in re.findall(re.compile(r'\w,?'), header):
         if field != '':
             field_matcher = re.compile(r'(?P<field>\w.*)({(?P<range>\d+(-\d+)?)})?(::(?P<function>media|sum|maximum|minimum))?')
@@ -19,8 +18,11 @@ def make_match(header: str):
 
             if field_dispatcher['range'] != '':
                 span = [int(x) for x in field_dispatcher['range'].split(',')]
-                field_dispatcher['range'] = range(span[0], range[0]+1) if len(span) == 1 else range(span[0], span[-1])
-
-
-            if field_dispatcher['function'] != '':
-                field_dispatcher['function'] = table[field_dispatcher['function']]
+                regex += (fr'(?P<{field}>(.*,?){{{span[0]}}})' if len(span) == 1 else fr'(?P<{field}>(.*,?){{{span[0]}-{span[-1]}}})')
+            else:
+                regex += fr'(?P<{field}>.*)'
+    
+    return regex
+            
+            # if f := field_dispatcher['function'] != '':
+            #     regex += fr'(?P<{field}_{f}>.*)'
