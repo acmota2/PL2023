6cc5b6d530c2aa89a93c4254ef87fa7be61c3f12
acmota2 <acmota2@gmail.com>
2023-03-14 11:05:01 +0000

    [TPC4]: minor adjustments (nada novo)

--- TPC4/main.py
+++ TPC4/main.py
@@ -1,20 +1,19 @@
 from sys import argv
-import csv_utils as csv
+import csv_utils as csv2
 from json import dumps
 import re
 
 if __name__ == '__main__':
     f = open(argv[1], 'r')
     h, *t = f.readlines()
-    raw_matcher, dispatcher = csv.make_match(h)
+    raw_matcher, dispatcher = csv2.make_match(h)
     matcher = re.compile(raw_matcher)
     json_file = open(f"./{argv[1].split('.')[0]}.json", "w")
     dicts = []
     for line in [x.rstrip('\n') for x in t]:
-        current = '\t{\n'
         if m := matcher.fullmatch(line):
             line_strs = m.groupdict()
-            maybe_valid = csv.field_validation(dispatcher, line_strs)
+            maybe_valid = csv2.field_validation(dispatcher, line_strs)
 
             if maybe_valid != None:
                 dicts.append(maybe_valid)
