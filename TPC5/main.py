4e2b3dc246fbd5291ada75f0f442c06af92414fc
acmota2 <acmota2@gmail.com>
2023-03-21 12:27:58 +0000

    [TPC5]: agora tem o 112

--- TPC5/main.py
+++ TPC5/main.py
@@ -12,6 +12,7 @@ coins_: dict[int] = {
 num_val: dict[str, int] = {
     r'^00\d+$': 150,
     r'^8(?!08)\d{8}$': 0,
+    r'112': 0,
     r'^808\d{6}$': 10,
     r'^9(1|2|3|6)\d{7}$': 25,
     r'^2\d{8}$': 25
@@ -20,6 +21,8 @@ num_val: dict[str, int] = {
 def number_checker(num: str) -> int:
     for k, v in num_val.items():
         if re.match(k, num):
+            if k == '112':
+                print('Chamada de emergência!')
             return v
     return -1
 
@@ -48,14 +51,14 @@ if __name__ == '__main__':
     while True:
         read = input('maq in touch > ')
         if re.match(r'^\s*LEVANTAR\s*$', read) and not levantado:
+            print('Telefone levantado. Insira moedas ou efetue uma chamada grátis.')
             levantado = True
         elif levantado:
             if m := re.fullmatch(r'^\s*MOEDA\s+(?P<coins>(.*,?)+)\s*$', read):
                 cur_bal += coin_checker(re.sub(r'\s+', r'', m.groupdict()['coins']))
-                print(f"Saldo atual: {(cur_bal/100):.2f}")
+                print(f"Saldo atual: {(cur_bal/100):.2f}€")
             
-            elif m := re.fullmatch(r'^\s*T\s*=\s*(?P<num>00\d+|\d{9})\s*$', read):
-                print(m.groupdict()['num'])
+            elif m := re.fullmatch(r'^\s*T\s*=\s*(?P<num>00\d+|\d{9}|112)\s*$', read):
                 cost: int = number_checker(m.groupdict()['num'])
                 if cost == -1:
                     print('Insira um número válido!')
@@ -63,11 +66,11 @@ if __name__ == '__main__':
                     print(f'Não tem saldo suficiente para efetuar esta operação!')
                 else:
                     cur_bal -= cost
-                print(f'Custo total da operação: {(cost/100):.2f}; Saldo atual: {(cur_bal/100):.2f}')
+                print(f'Custo total da operação: {(cost/100):.2f}€; Saldo atual: {(cur_bal/100):.2f}€')
             
             elif re.fullmatch(r'\s*POUSAR\s*', read):
                 if cur_bal != 0:
-                    print(f'Troco total: {(cur_bal/100):.2f}; ', end='')
+                    print(f'Troco total: {(cur_bal/100):.2f}€; ', end='')
                     change_maker(cur_bal)
                 print('Obrigado e volte sempre!')
                 break
