940b1fe49a335018be8b39b5473160c3b339e341
acmota2 <acmota2@gmail.com>
2023-03-28 00:40:58 +0100

    [ADD]: TPC6 done

--- TPC6/main.py
+++ TPC6/main.py
@@ -3,7 +3,7 @@ import sys
 
 tokens = (
     # comment
-    'COMMENT'
+    'COMMENT',
     # abre e fecha
     'COMMENT_L','COMMENT_R','L_BRACKET','R_BRACKET','L_BRACE','R_BRACE','L_PAR','R_PAR',
     # tipos
@@ -11,27 +11,27 @@ tokens = (
     # loops
     'WHILE','FOR',
     # declaracoes
-    'VAR','ASSIGN','NUM','SEMICOLON','COMMA'
+    'VAR','ASSIGN','NUM','SEMICOLON','COMMA',
     # aritmetica
-    'SUM','MUL','DIV','SUB','RANGE'
+    'SUM','MUL','DIV','SUB','RANGE',
     # cond
-    'EQUAL','DIF','NOT','LESS','MORE','LESS_E','MORE_E', 'IN'
+    'EQUAL','DIF','NOT','LESS','MORE','LESS_E','MORE_E', 'IN',
     # func
     'FUNCTION','PROGRAM'
 )
 
-t_ignored = r' \t\n\r'
+t_ignore = f' \n\t\r'
 # comments
-t_COMMENT = r'//.*'
-# abre e fecha
-t_COMMENT_L = r'/\*'
+t_COMMENT_L = r'(\/\*)(.*)(?=(\*\/))'
 t_COMMENT_R = r'\*/'
-t_L_BRACKET = r'['
-t_R_BRACKET = r']'
+t_COMMENT = r'\/\/.*'
+# abre e fecha
+t_L_BRACKET = r'\['
+t_R_BRACKET = r'\]'
 t_L_BRACE = r'{'
 t_R_BRACE = r'}'
-t_L_PAR = r'('
-t_R_PAR = r')'
+t_L_PAR = r'\('
+t_R_PAR = r'\)'
 # tipos
 t_CHAR = r'char'
 t_SHORT = r'short'
@@ -41,17 +41,6 @@ t_STRING = r'string'
 # loops
 t_WHILE = r'while'
 t_FOR = r'for'
-# declaracoes
-t_VAR = r'[_a-z]\w*'
-t_ASSIGN = r'='
-
-def t_NUM(t):
-    r'[+\-]?\d+'
-    t.value = int(t.value)
-    return t
-
-t_SEMICOLON = r';'
-t_COMMA = r','
 # artimetica
 t_SUM = r'\+'
 t_MUL = r'\*'
@@ -69,15 +58,31 @@ t_MORE_E = r'>='
 t_IN = r'in'
 # func
 t_FUNCTION = r'function'
-t_PROGRAM = r'PROGRAM'
+t_PROGRAM = r'program'
+# declaracoes
+t_VAR = r'[_a-z]\w*'
+t_ASSIGN = r'='
+
+def t_NUM(t):
+    r'[+\-]?\d+'
+    t.value = int(t.value)
+    return t
+
+t_SEMICOLON = r';'
+t_COMMA = r','
 
 def t_error(t):
     print('Carater não válido')
     t.lexer.skip(1)
 
 if __name__ == '__main__':
-    data = sys.stdin.read() if len(sys.argv) == 0 else open(sys.argv).read()
-    lexer = lex.input(data)
+    data = sys.stdin.read() if len(sys.argv) == 1 else open(sys.argv).read()
+    lexer = lex.lex()
+    lexer.input(data)
     toks = []
     for tok in lexer:
         toks.append(tok)
+    print(f'''
+Tamanho da lista: {len(toks)}
+Lista:
+{toks}''')
